fb0710fc73328c79a1843b8d6089bb147042bcc1
Dean Brundage <dean@deanandadie.net>
2015-02-09 16:43:06 -0600

    moving monitor to bin and shit to lib

---
+++ lib/bcs_babysitter.rb
@@ -0,0 +1,64 @@
+module NRB
+
+  class BCSBabysitter
+
+    attr_accessor :pause
+
+    def initialize(bcs: nil, sns: nil, topic_arn: nil)
+      raise ArgumentError.new("Need bcs: parameter") if bcs.nil?
+      raise ArgumentError.new("Need sns: parameter") if sns.nil?
+      raise ArgumentError.new("Need topic_arn: parameter") if topic_arn.nil?
+
+      self.bcs = bcs
+      self.pause ||= 5
+      self.sns = sns
+      self.topic_arn = topic_arn
+    end
+
+
+    def monitor
+      while true do
+        sleep pause unless previous.nil?
+        begin
+          self.current = bcs.temp_probes
+          changes = setpoint_changes
+          unless changes.empty?
+            message = "#{Time.now} #{changes.inspect}"
+            puts message
+            sns.publish topic_arn: topic_arn,
+                        message: message
+          end
+          self.previous = current
+        rescue Aws::SNS::Errors::ServiceError => e
+          puts "#{Time.now} #{e}"
+        rescue Faraday::ConnectionFailed => e
+          message = "#{Time.now} #{e}"
+          puts message
+          sns.publish topic_arn: topic_arn,
+                      message: message
+        end
+      end
+    end
+
+
+    def setpoint_changes
+      changes = []
+      return changes if previous.nil?
+      previous.each_with_index do |probe,i|
+        if probe.name != current[i].name
+          changes << "#{probe.name} changed names to #{current[i].name}"
+        end
+        if probe.setpoint != current[i].setpoint
+          changes << "#{probe.name} changed setpoint from #{probe.setpoint} to #{current[i].setpoint}"
+        end
+      end
+      changes
+    end
+
+  private
+
+    attr_accessor :bcs, :current, :previous, :sns, :topic_arn
+
+  end
+
+end
